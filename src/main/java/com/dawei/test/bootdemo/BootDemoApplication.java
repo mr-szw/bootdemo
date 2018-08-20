147f04c97fa2aae480514445acd65ba5238ae230
Dawei <15554485117@163.com>
2018-08-20 23:47:10 +0800

    ActiveMq

--- src/main/java/com/dawei/test/bootdemo/BootDemoApplication.java
+++ src/main/java/com/dawei/test/bootdemo/BootDemoApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms       //开启消息功能
 @EnableCaching    //开启缓存功能
 @ComponentScan(value = "com.dawei.test.bootdemo")
 public class BootDemoApplication {
